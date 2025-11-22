1e041cf70c136d1fec73dfbd1a1077a8d031cbf6
deepuhacker9-source <deepuhacker9@gmail.com>
2025-11-22 21:20:36 +0530

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -5,10 +5,7 @@ export default function Signup() {
   const signupGoogle = async () => {
     try {
       const { error } = await supabase.auth.signInWithOAuth({
-  provider: "google",
-  options: {
-    redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`
-  }
+  provider: "google"
 });
 
       if (error) {
