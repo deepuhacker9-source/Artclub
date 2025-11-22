8e923c1263fb0bb38a33ddde40a567c89227a14f
deepuhacker9-source <deepuhacker9@gmail.com>
2025-11-22 21:21:11 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -4,10 +4,7 @@ import { supabase } from "../lib/supabaseClient";
 export default function Login() {
   const loginWithGoogle = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
-  provider: "google",
-  options: {
-    redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`
-  }
+  provider: "google"
 });
 
     if (error) {
